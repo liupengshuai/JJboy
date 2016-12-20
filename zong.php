33b67ac430cd3ab4deed120b238877b662403280
liupeng <18710164878@163.com>
2016-12-20 10:24:50 +0800

    地理位置

--- zong.php
+++ zong.php
@@ -33,7 +33,7 @@ $signPackage = $jssdk->GetSignPackage();
 	</head>
 	<body>
 		
-		<main>
+		<!-- <main>
 			<div class = "home"></div>
 			<div class = "market"></div>
 			<div class = "order"></div>
@@ -74,10 +74,12 @@ $signPackage = $jssdk->GetSignPackage();
 					</figcaption>
 				</figure></a>
 			</div>
-		</footer>
+		</footer> -->
+		<button onclick="f1"></button>
+
+		
 	</body>
 <script type="text/javascript">
-
  wx.config({
     debug: true,
     appId: '<?php echo $signPackage["appId"];?>',
@@ -120,5 +122,17 @@ $signPackage = $jssdk->GetSignPackage();
         'openCard'
       ]
   });
+ 	function f1(){
+ 		wx.getLocation({
+    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+    success: function (res) {
+        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+        var speed = res.speed; // 速度，以米/每秒计
+        var accuracy = res.accuracy; // 位置精度
+    }
+});
+ 	}
+ 	
 </script>
 </html>
